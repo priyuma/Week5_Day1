4aefd215d8f92eaf4a3ffa84f143d77ac473d143
priyuma <142703699+priyuma@users.noreply.github.com>
2023-09-23 23:53:37 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,32 @@
+package week5.Day1.Testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CreateLead extends BaseClass{
+@Test
+	public void createLeadTC() {
+		
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Subraja");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Subi");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+		String text = driver.findElement(By.id("viewLead_companyName_sp")).getText();
+		if (text.contains("Testleaf")) {
+			System.out.println("Lead created successfully");
+		}
+		else {
+			System.out.println("Lead is not created");
+		}
+		driver.close();
+
+
+	}
+
+}
